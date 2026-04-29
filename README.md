8b5b042883fa0ca8bce06bdebe7812cb8783d365
Tony Bark <35226681+tonytins@users.noreply.github.com>
2026-04-28 23:45:09 -0400

    Added help descriptions
    - Expanded README
    - Make sure desktop entry goes to the right place
    - If on macOS or Windows, print the entry and path

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # DeskIcon
 
-DeskIcon is a simple CLI tool for Linux that streamlines the process of making desktop entries.
+DeskIcon is a simple CLI tool for Linux that streamlines the process of making desktop entries. I have *no idea* if something like this already exists. I simply created it because making desktop entries for some applications that don't have packages was growing rather frustrating.
+
+## License
+
+I license this project under the GPL-3.0 license - see [LICENSE](LICENSE) for details.
--- Sources/DesktopIcon/DesktopIcon.swift
+++ Sources/DesktopIcon/DesktopIcon.swift
@@ -3,61 +3,74 @@ import Foundation
 
 @main
 struct DesktopIcon: ParsableCommand {
-    @Option
+    /// Help descriptions (and VSCode debug example) was grabbed from the Arch Wiki
+    /// https://wiki.archlinux.org/title/Desktop_entries
+    @Option(name: .shortAndLong, help: "The version of the desktop entry specification to which this file conforms")
     var version: String = "1.0"
 
-    @Option
+    @Option(name: .shortAndLong, help: "The name of the application")
     var name: String
 
-    @Option
-    var comment: String = ""
+    // @Option(name: .shortAndLong, help: "A comment which can/will be used as a tooltip")
+    // var comment: String = ""
 
-    @Option
+    @Option(name: .shortAndLong, help: "The path to the folder in which the executable is run")
     var path: String
 
-    @Option
+    @Option(name: .shortAndLong, help: "The executable of the application, possibly with arguments.")
     var exec: String
 
-    @Option
+    @Option(name: .shortAndLong, help: "The name of the icon that will be used to display this entry")
     var icon: String
 
-    @Option
-    var categories: String = ""
-    
-    @Flag
-    var dryRun: Bool = true // For debugging
+    @Option(name: .shortAndLong, help: "Describes the categories in which this entry should be shown")
+    var categories: String
+
+    @Flag(name: .shortAndLong, help: "Describes whether this application needs to be run in a terminal or not")
+    var terminal: Bool = false
+
+    @Flag(name: .shortAndLong)
+    var dryRun: Bool = false
+
+    func printEntry(content: String, directory: String) {
+        print(content)
+        print(directory)
+    }
 
     mutating func run() throws {
         let environment = ProcessInfo.processInfo.environment
+        let loc = ".local//share//applications"
         let entry = """
         [Desktop Entry]
         Type=Application
         Version=\(version)
         Name=\(name)
-        Comment=\(comment)
         Path=\(path)
         Exec=\(exec)
         Icon=\(icon)
-        Terminal=false
+        Terminal=\(terminal)
         Categories=\(categories)
         """
- 
-        if !dryRun
-        {
-            if environment["HOME"] != nil {
-                 do {
-                     try entry.write(
-                        toFile: "\(environment["HOME"]!)//Desktop",
-                        atomically: true,
-                        encoding: .utf8
-                     )
-                 } catch {
-                     print("Error: \\(error)")
-                 }
-             }
-        } else {
-            print(entry)
-            print("\(environment["HOME"]!)//Desktop")
-        }
+        let file = "\(environment["HOME"]!)//\(loc)//\(name).desktop"
+
+        #if os(Linux)
+            if !dryRun {
+                if environment["HOME"] != nil {
+                    do {
+                        try entry.write(
+                            toFile: file,
+                            atomically: true,
+                            encoding: .utf8
+                        )
+                    } catch {
+                        print("Error: \\(error)")
+                    }
+                }
+            } else {
+                printEntry(content: entry, directory: file)
+            }
+        #else
+            printEntry(content: entry, directory: file)
+        #endif
     }
 }
